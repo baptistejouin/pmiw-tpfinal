b7224afd91b5b2fbc588cd29b764fd63b804b79e
baptistejouin <baptiste.jouin44@gmail.com>
2024-10-09 21:42:27 -0300

    simplificar y mejorar el código

--- pmiw_tpfinal.js
+++ pmiw_tpfinal.js
@@ -1,55 +1,205 @@
-let canvasWidth = 800;
-let canvasHeight = 400;
-let fontSize = canvasHeight * 0.045;
-let margin = fontSize / 2;
-let currentDialog = 0;
-let numSteps = 11;
-let strings;
-let images = [];
-let buttons = [];
-let textboxes = [];
+let anchoPantalla = 640;
+let altoPantalla = 480;
+let tamanoFuente = 16;
+let pantalla;
+let rama;
+let textos = [];
+let imagenes = [];
+let sonidos = [];
+let rutasSonidos = [];
+let rutasImagenes = [
+  "/assets/img/01.png",
+  "/assets/img/02.png",
+  "/assets/img/03.png",
+  "/assets/img/04.png",
+  "/assets/img/05.png",
+  "/assets/img/06.png",
+  "/assets/img/07.png",
+  "/assets/img/08.png",
+  "/assets/img/09.png",
+  "/assets/img/10.png",
+  "/assets/img/11.png",
+];
+
+let mitadAnchoPantalla = anchoPantalla / 2;
+let anchoBoton = mitadAnchoPantalla - 75;
+let posicionYBoton = altoPantalla - tamanoFuente * 3.5;
+let altoTexto = tamanoFuente * 4;
+let altoBoton = tamanoFuente * 3;
+
+let textoPrincipal = {
+  posicionX: 50,
+  posicionY: altoPantalla - tamanoFuente * 8,
+  ancho: anchoPantalla - 100,
+  alto: altoTexto,
+};
+
+let botonIzquierdo = {
+  posicionX: 70,
+  posicionY: posicionYBoton,
+  ancho: anchoBoton,
+  alto: altoBoton,
+};
+
+let botonDerecho = {
+  posicionX: anchoPantalla - anchoBoton - 70,
+  posicionY: posicionYBoton,
+  ancho: anchoBoton,
+  alto: altoBoton,
+};
 
 function preload() {
-  strings = loadStrings("./assets/strings.txt");
-  for (let i = 1; i <= numSteps; i++) {
-    images.push(loadImage(`./assets/img/${i.toString().padStart(2, "0")}.png`));
+  // precargar textos
+  textos = loadStrings("./assets/textos.txt");
+
+  // precargar imágenes
+  for (let i = 0; i < rutasImagenes.length; i++) {
+    imagenes.push(loadImage(rutasImagenes[i]));
+  }
+
+  // precargar sonidos
+  soundFormats("mp3");
+  for (let i = 0; i < rutasSonidos.length; i++) {
+    sonidos.push(loadSound(rutasSonidos[i]));
   }
 }
 
 function setup() {
-  createCanvas(canvasWidth, canvasHeight);
-  createNewButton("before", { x: 200, y: 350 }, 100, 25, () => {
-    if (currentDialog != 0) {
-      currentDialog--;
-    }
-  });
-  createNewButton("next", { x: 350, y: 350 }, 100, 25, () => {
-    if (currentDialog + 1 < numSteps) {
-      currentDialog++;
-      return;
-    }
-    currentDialog = 0;
-  });
-  createNewTextbox("", { x: 200, y: 300 }, 350, 50);
+  createCanvas(anchoPantalla, altoPantalla);
+  textSize(tamanoFuente);
+  textAlign(CENTER, CENTER);
+  noStroke();
+
+  pantalla = "flujo"; // hay 3 pantallas: inicio, creditos, flujo
+  rama = 0; // cada 3 líneas, hay un nuevo texto principal, una línea para la primera opción y otra para la segunda
 }
 
 function draw() {
   background(200);
-  cursor("default");
-  textSize(fontSize);
 
-  image(images[currentDialog], 125, 10, 540, 360);
-
-  drawBoxes();
-  updateCursor();
-
-  textboxes[0].text = strings[currentDialog];
+  if (pantalla === "inicio") {
+    dibujarInicio();
+  }
+  if (pantalla === "creditos") {
+    dibujarCreditos();
+  }
+  if (pantalla === "flujo") {
+    dibujarFlujo();
+  }
 }
 
 function mousePressed() {
-  handleButtonClick();
+  // verificar si está en el botón izquierdo
+  if (
+    mouseX > botonIzquierdo.posicionX &&
+    mouseX < botonIzquierdo.posicionX + botonIzquierdo.ancho &&
+    mouseY > botonIzquierdo.posicionY &&
+    mouseY < botonIzquierdo.posicionY + botonIzquierdo.alto &&
+    textos[rama + 1] !== ""
+  ) {
+    /* 
+      Extrae los 2 primeros caracteres de la línea correspondiente a la opción 
+      (estos caracteres representan el número de línea de la siguiente rama). 
+      Luego, convierte esos caracteres en un número utilizando parseInt 
+      (ya que inicialmente son de tipo texto). 
+
+      Después, resta 1 para obtener el índice correcto dentro del array de textos. 
+      Podríamos empezar desde 0 para evitar esta resta, pero en la visualización 
+      de Processing la numeración de las líneas comienza en 1, lo que hace que sea 
+      más fácil ubicarse al crear el árbol en textos.txt.
+    */
+    rama = parseInt(textos[rama + 1].slice(0, 2)) - 1;
+  }
+
+  // verificar si está en el botón derecho
+  if (
+    mouseX > botonDerecho.posicionX &&
+    mouseX < botonDerecho.posicionX + botonDerecho.ancho &&
+    mouseY > botonDerecho.posicionY &&
+    mouseY < botonDerecho.posicionY + botonDerecho.alto &&
+    textos[rama + 2] !== ""
+  ) {
+    rama = parseInt(textos[rama + 2].slice(0, 2)) - 1;
+  }
 }
 
-function updateCursor() {
-  handleCursorChange();
+function dibujarFlujo() {
+  // imágenes
+  // image(imagenes[rama / 3], 0, 0, anchoPantalla, altoPantalla); // necesito 20 imágenes
+
+  // texto principal
+  fill("rgba(0, 0, 0, 0.25)");
+  rect(
+    textoPrincipal.posicionX,
+    textoPrincipal.posicionY,
+    textoPrincipal.ancho,
+    textoPrincipal.alto,
+    2
+  );
+  fill(255);
+  text(
+    textos[rama],
+    textoPrincipal.posicionX,
+    textoPrincipal.posicionY,
+    textoPrincipal.ancho,
+    textoPrincipal.alto
+  );
+
+  // opción 1 (izquierda)
+  if (textos[rama + 1] !== "") {
+    fill("rgba(0, 0, 0, 0.25)");
+    rect(
+      botonIzquierdo.posicionX,
+      botonIzquierdo.posicionY,
+      botonIzquierdo.ancho,
+      botonIzquierdo.alto,
+      2
+    );
+    fill(255);
+    text(
+      textos[rama + 1].slice(2),
+      botonIzquierdo.posicionX,
+      botonIzquierdo.posicionY,
+      botonIzquierdo.ancho,
+      botonIzquierdo.alto
+    );
+  }
+
+  // opción 2 (derecha)
+  if (textos[rama + 2] !== "") {
+    fill("rgba(0, 0, 0, 0.25)");
+    rect(
+      botonDerecho.posicionX,
+      botonDerecho.posicionY,
+      botonDerecho.ancho,
+      botonDerecho.alto,
+      2
+    );
+    fill(255);
+    text(
+      /*
+        Extrae el texto desde el tercer carácter de la línea correspondiente 
+        a la segunda opción de la rama actual. Los dos primeros caracteres 
+        representan el número de línea de la siguiente rama, por lo que se omiten, 
+        dejando solo el texto de la opción en sí. 
+      */
+      textos[rama + 2].slice(2),
+      botonDerecho.posicionX,
+      botonDerecho.posicionY,
+      botonDerecho.ancho,
+      botonDerecho.alto
+    );
+  }
+
+  // debug
+  fill(255);
+  text(`rama línea : ${rama + 1} | imagen: ${rama / 3}`, 10, tamanoFuente);
+}
+
+function dibujarCreditos() {
+  text("Créditos", 0, tamanoFuente);
+}
+
+function dibujarInicio() {
+  text("Inicio", 0, tamanoFuente);
 }
